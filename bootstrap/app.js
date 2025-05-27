afb9926671d5b3690931eab250ca397cfe0e3b53
Yashwl2005 <yashdeepakwl@gmail.com>
2025-05-27 15:41:56 +0530

    Add files via upload

---
+++ bootstrap/app.js
@@ -0,0 +1,13 @@
+let i = 1 ;
+for (let i = 1; i <= 15; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+console.log("Even numbers from 1 to 15:");
+let j = 1;
+for (let j = 1; j <= 15; j++) {
+    if (j % 2 === 0) {
+        console.log(j);
+    }
+}
